6d50e0152e8d9dcb86d4a738cbca4e894ca1c469
moeizadil <moeizadil786@gmail.com>
2023-07-24 02:24:31 +0500

    changes

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,9 +1,20 @@
 /// <reference types = "cypress"/>
 //In this test file I have automated login functionality using Cypress
+
+beforeEach(()=>{
+    cy.visit('/web/index.php/auth/login')
+})
+//it.only will run only particular test case. Only is the test function inside Mocha
+//it.skip will skip the test case
 it("should login to the website", () =>{
-    cy.login('admin','admin123')
-    cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
-    cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    cy.fixture('example').then(function(data){
+        const credentials = data
+        cy.login(credentials.username,credentials.password)
+        cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dashboard')
+        cy.url().should('eq','https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index')
+    })
+
+   
 })
 it('should not login to website', ()=>{
     cy.login('admin','admin1234')
